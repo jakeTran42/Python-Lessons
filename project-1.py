c0370f8c4eef1f221400c3d7a3ee943604ddefc4
Jake Tran <jake.tranehs@gmail.com>
2020-04-16 11:59:40 -0700

    Create project-1.py

---
+++ project-1.py
@@ -0,0 +1,49 @@
+### Homework #7
+
+Use Repl.it python editor to write your program and save the file to your computer when youre done.
+
+https://repl.it/languages/python3
+
+1. Write a python program that merges two python dictionary.
+
+Example
+
+```
+dict_1 = {'a': 100, 'b': 200}
+dict_2 = {'x': 300, 'y': 200}
+.
+.
+>>>dict_3 = {'x': 300, 'y': 200, 'a': 100, 'b': 200}
+```
+
+2. Write a python program to get the maximum and minimum value in a dictionary.
+
+Example
+
+```
+my_dict = {'x':500, 'y':5874, 'z': 560}
+
+>>> max: 5874, min: 500
+```
+
+### Stretch Challenge
+
+3. Write a python program to list all keys-value pair of a dictionary in order from least to greatest numerically. Display it however you like.
+
+Exmample
+
+```
+dict = {'a': 100, 'b': 200, 'c': 50, 'd': 875, 'e': 105}
+
+>>> {'c': 50, 'a': 100, 'e':105, 'b': 200, 'd': 875}
+
+Resources
+
+https://www.w3schools.com/python/python_arrays.asp
+
+https://automatetheboringstuff.com/2e/chapter4/
+
+https://www.programiz.com/python-programming/array
+
+https://www.tutorialspoint.com/python_data_structure/python_sorting_algorithms.htm
+```
