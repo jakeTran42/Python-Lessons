ea94b09a2f00181f28efac2e3237a1e5ccdf6d05
Jake Tran <jake.tranehs@gmail.com>
2020-04-24 11:42:08 -0700

    Create homework11.py

---
+++ homework11.py
@@ -0,0 +1,32 @@
+### Homework #7
+
+Use Repl.it python editor to write your program and save the file to your computer when youre done.
+
+https://repl.it/languages/python3
+
+1. Write a Python program to read an entire text file. Create the file yourself that can have anything in it with the ending extention .txt
+
+Example
+
+```
+reading from hello.txt
+
+>>> Hello World
+```
+
+2. Write a string to a file and save it to a directory.
+
+3. Write a Python program to read a file line by line and store it into a list. 
+
+4. From the file you created. Write or add in new code that will add in (append) new words or sentences to the file
+
+
+Resources
+
+https://www.geeksforgeeks.org/file-handling-python/
+
+https://automatetheboringstuff.com/2e/chapter9/
+
+https://www.tutorialspoint.com/python/python_files_io.htm
+
+https://www.youtube.com/watch?v=ZeLocPccJvg
